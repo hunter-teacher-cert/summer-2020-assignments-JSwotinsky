21807d3c1df2fac94b89f62f1a80410c78173581
Jonathan Swotinsky <JSwotinskyIEC@gmail.com>
2020-07-10 22:34:45 -0400

    More progress on MyStack() class. size() method issue needs resolution (length of stack does not decrement after pop()).

--- classwork/Day08/MyStackDriver.java
+++ classwork/Day08/MyStackDriver.java
@@ -24,6 +24,14 @@ public class MyStackDriver {
 	System.out.println("\nTest for MyStack() constructor:");
 	System.out.println(stack);
 	System.out.println();
+		
+	// Test 1 for MyStack() size() method:
+	System.out.println("Test 1 for MyStack() size() method: " + "\t" + stack.size());	
+
+	
+	// Test 1 for MyStack() isEmpty() method:
+	System.out.println("Test 1 for MyStack() isEmpty() method: " + "\t" + stack.isEmpty() + "\n");
+	
 	
 	// Test for MyStack push() method:
 	System.out.println("Test for MyStack() push() method:");
@@ -36,17 +44,35 @@ public class MyStackDriver {
 	System.out.println(stack);
 	System.out.println();
 
+	
+	// Test 2 for MyStack() size() method:
+	System.out.println("Test 2 for MyStack() size() method: " + "\t" + stack.size());
+	
+	
+	// Test 2 for MyStack() isEmpty() method:
+	System.out.println("Test 2 for MyStack() isEmpty() method: " + "\t" + stack.isEmpty() + "\n");
+
+
 	// Test for MyStack pop() method:
 	System.out.println("Test for MyStack() pop() method:");
 	System.out.println("Current state of stack: " + stack);
 	String s = stack.pop();
 	System.out.println("Stack state after pop(): " + stack + "\t" + "Element returned: " + s);
 	System.out.println();
+
+	
+	// Test 3 for MyStack() size() method (after pop):
+	System.out.println("Test 3 for MyStack() size() method (after pop): " + "\t" + stack.size());
 	
 	
+	// Test 3 for MyStack() isEmpty() method (after pop):
+	System.out.println("Test 3 for MyStack() isEmpty() method (after pop): " + "\t" + stack.isEmpty() + "\n");
+
 	
-	
-	
+	// Test for MyStack top() method:
+	System.out.println("Test for MyStack() top() method:");
+	System.out.println("Current state of stack: " + stack + "\t" + "Top element in stack: " + stack.top());
+	System.out.println();
 	
 	}// end main
 	
